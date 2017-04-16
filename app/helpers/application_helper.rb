c02435bd4e2d747e83ced3185f87837099a1e624
Takayuki Doi <doi@retador.net>
2017-04-16 22:23:03 +0900

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,5 +15,9 @@ module ApplicationHelper
         content_tag(:p, greeting, class: "source-greeting")
        end
   end
-end
 
+  def copyright_generator
+    DevcampViewTool::Renderer.copyright 'Jordan Haudgens', 'All rights reserved'
+  end
+
+end
