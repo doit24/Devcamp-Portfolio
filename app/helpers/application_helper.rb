d0e0dd9330c94b61448f3b35792ab7b1080ed057
Takayuki Doi <doi@retador.net>
2017-04-24 10:05:53 +0900

    Integrated image background for app layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Jordan Haudgens', 'All rights reserved'
+    DevcampViewTool::Renderer.copyright 'Retador Co., Ltd.', 'All rights reserved'
   end
 
   def nav_items
