e5badf85a90560bbca149142c8830b119e0f8f19
Takayuki Doi <doi@retador.net>
2017-04-29 23:05:01 +0900

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,5 +59,19 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+     js add_gritter(msg, title:"Takayuki Doi Portfolio", sticky: false)
+  end
+
+
+
+
 end
 
