1f1446e86bba5445cd48db5dc971a721d29fd087
Takayuki Doi <doi@retador.net>
2017-05-06 17:23:01 +0900

    Integreated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://ceramic-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://ceramic-portfolio.herokuapp.com/cable"
 end
