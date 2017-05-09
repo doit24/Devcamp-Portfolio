5f6cecc4f6ea57583859a5895731f934ad2a8693
小野 慶太 <dorokei@gmail.com>
2017-05-10 00:52:39 +0900

    ローカル起動まで

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 20170504063414) do
     t.text     "thumb_image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "poisition"
     t.integer  "position"
   end
 
